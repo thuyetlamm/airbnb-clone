1b6f09b2a1708cde84e7f720614057e2426d0e17
LamNguyen <thuyetlamm@gmail.com>
2022-09-03 19:38:00 +0700

    cardBooking and handle task click count

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import placeListReducer from '~/components/PlacesList/placeListSlice';
 import categoryReducer from '~/components/NavigationBar/categorySlice';
+import counterReducer from '~/components/features/Counter/counterSlice';
 const rootReducer = {
   placeList: placeListReducer,
   category: categoryReducer,
+  counter: counterReducer,
 };
 
 const store = configureStore({
